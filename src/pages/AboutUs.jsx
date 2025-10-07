437b56bd36cf880376b4a397dbfcfbc9cdc83d7e
nefer group <info.nefergroup@gmail.com>
2025-10-08 03:14:00 +0530

    Update AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -17,6 +17,9 @@ import Partner from "../components/aboutPage/Partner";
 import logo from "../assets/images/logo.png";
 import { useState } from "react";
 
+// Import the same CSS file
+import "../components/header/mobile-menu.css";
+
 AboutUs.propTypes = {};
 
 function AboutUs(props) {
@@ -46,38 +49,96 @@ function AboutUs(props) {
             </Link>
           </div>
 
-          <div className="mobile-menu-simple">
-  <nav className="main-nav mobile">
-    <ul id="menu-primary-menu" className="menu">
-      <li className="menu-item">
-        <Link to="/" className="iteam-menu" onClick={handleMobile}>
-          Home
-        </Link>
-      </li>
-      <li className="menu-item">
-        <Link to="/joblist_v1" className="iteam-menu" onClick={handleMobile}>
-          Find Jobs
-        </Link>
-      </li>
-      <li className="menu-item">
-        <Link to="/aboutus" className="iteam-menu" onClick={handleMobile}>
-          About
-        </Link>
-      </li>
-      <li className="menu-item">
-        <Link to="/contactus" className="iteam-menu" onClick={handleMobile}>
-          Contact
-        </Link>
-      </li>
-    </ul>
-  </nav>
+          {/* Simplified Mobile Menu - Same as other pages */}
+          <div className="mobile-menu-content">
+            <nav className="main-nav mobile">
+              <ul id="menu-primary-menu" className="menu">
+                <li className="menu-item">
+                  <Link to="/" className="iteam-menu" onClick={handleMobile}>
+                    Home
+                  </Link>
+                </li>
 
-  <div className="header-customize-item button">
-    <Link to="/" onClick={handleMobile}>Upload Resume</Link>
-  </div>
-</div>
+                <li className="menu-item">
+                  <Link to="/joblist_v1" className="iteam-menu" onClick={handleMobile}>
+                    Find Jobs
+                  </Link>
+                </li>
+
+                <li className="menu-item current-item">
+                  <Link to="/aboutus" className="iteam-menu" onClick={handleMobile}>
+                    About Us
+                  </Link>
+                </li>
+
+                <li className="menu-item">
+                  <Link to="/contactus" className="iteam-menu" onClick={handleMobile}>
+                    Contact Us
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="header-customize-item button">
+            <Link to="/" onClick={handleMobile}>Upload Resume</Link>
+          </div>
+
+          <div className="mobile-footer">
+            <div className="icon-infor d-flex aln-center">
+              <div className="icon">
+                <span className="icon-call-calling">
+                  <span className="path1"></span>
+                  <span className="path2"></span>
+                  <span className="path3"></span>
+                  <span className="path4"></span>
+                </span>
+              </div>
+              <div className="content">
+                <p>Need help? 24/7</p>
+                <h6>
+                  <Link to="tel:0123456678">001-1234-88888</Link>
+                </h6>
+              </div>
+            </div>
+            <div className="wd-social d-flex aln-center">
+              <ul className="list-social d-flex aln-center">
+                <li>
+                  <Link to="#">
+                    <i className="icon-facebook"></i>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#">
+                    <i className="icon-linkedin2"></i>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#">
+                    <i className="icon-twitter"></i>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#">
+                    <i className="icon-pinterest"></i>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#">
+                    <i className="icon-instagram1"></i>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#">
+                    <i className="icon-youtube"></i>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
+      
       <Header2 clname="actPage5" handleMobile={handleMobile} />
       <Breadcrumb title="About Us" />
       <Review />
