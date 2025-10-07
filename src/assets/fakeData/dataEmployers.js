dce846f3c70a0a1e46dce0de885773d18261e23a
badusha5618 <badushaperingala@gmail.com>
2025-10-07 16:13:28 +0530

    da

--- src/assets/fakeData/dataEmployers.js
+++ src/assets/fakeData/dataEmployers.js
@@ -1,58 +1,40 @@
-import img1 from "../images/logo-company/croatia.png";
-import img2 from "../images/logo-company/latvia.png";
-import img3 from "../images/logo-company/serbia.jpg";
-import img4 from "../images/logo-company/germany.jpg";
-import img5 from "../images/logo-company/qatar.png";
-import img6 from "../images/logo-company/cty14.png";
-import img7 from "../images/logo-company/cty15.png";
-import img8 from "../images/logo-company/cty9.png";
-import img9 from "../images/logo-company/cty12.png";
-import img10 from "../images/logo-company/cty6.png";
-import img11 from "../images/logo-company/cty7.png";
-import img12 from "../images/logo-company/cty3.png";
-
-
-
 const dataEm = [
   {
     id: 1,
-    img: img1,
+    img: "/images/logo-company/croatia.png",
     title: "Croatia",
     map: "Croatia",
     job: "0 job openings",
   },
   {
     id: 2,
-    img: img2,
+    img: "/images/logo-company/latvia.png",
     title: "Latvia",
     map: "Latvia",
     job: "1 job openings",
   },
   {
     id: 3,
-    img: img3,
+    img: "/images/logo-company/serbia.jpg",
     title: "Serbia",
     map: "Serbia",
     job: "7 job openings",
   },
   {
     id: 4,
-    img: img4,
+    img: "/images/logo-company/germany.jpg",
     title: "Germany",
     map: "Germany",
     job: "3 job openings",
   },
   {
     id: 5,
-    img: img5,
+    img: "/images/logo-company/qatar.png",
     title: "Qatar",
     map: "Qatar",
     job: "2 job openings",
   },
-  
-  
-  
-  
+
 ];
 
 export default dataEm;
