ab534ae2d0c72ecff5fe4a13ff50282e7f9419fb
nefer group <info.nefergroup@gmail.com>
2025-10-08 04:16:25 +0530

    Update dataPartner.js

--- src/assets/fakeData/dataPartner.js
+++ src/assets/fakeData/dataPartner.js
@@ -1,30 +1,13 @@
-import img1 from "../assets/images/partners/1.jpeg";
-import img2 from "../assets/images/partners/2.jpeg";
-import img3 from "../assets/images/partners/3.jpeg";
-import img4 from "../assets/images/partners/4.jpeg";
-
-
-
+import img1 from "../images/partners/1.jpeg";
+import img2 from "../images/partners/2.jpeg";
+import img3 from "../images/partners/3.jpeg";
+import img4 from "../images/partners/4.jpeg";
 
 const dataPartner = [
-    {
-        id: 1,
-        img: img1
-    },
-    {
-        id: 2,
-        img: img2
-    },
-    {
-        id: 3,
-        img: img3
-    },
-    {
-        id: 4,
-        img: img4
-    },
-   
-    
-]
+  { id: 1, img: img1 },
+  { id: 2, img: img2 },
+  { id: 3, img: img3 },
+  { id: 4, img: img4 },
+];
 
 export default dataPartner;
